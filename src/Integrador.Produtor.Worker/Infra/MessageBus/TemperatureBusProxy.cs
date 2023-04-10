3006512cca1b32db2d9fa62408244b4b755cfe1c
jfbueno <buenojeferson@outlook.com>
2023-04-10 18:59:54 -0300

    Remoção de código de compreensão não usado

--- src/Integrador.Produtor.Worker/Infra/MessageBus/TemperatureBusProxy.cs
+++ src/Integrador.Produtor.Worker/Infra/MessageBus/TemperatureBusProxy.cs
@@ -1,9 +1,6 @@
 ﻿using Integrador.Common.MessageBus.Contracts;
 using Integrador.Produtor.Worker.Infra.Database;
 using MassTransit;
-using System.IO.Compression;
-using System.Text;
-using System.Text.Json;
 
 namespace Integrador.Produtor.Worker.Infra.MessageBus;
 
@@ -31,25 +28,5 @@ internal sealed class TemperatureBusProxy
                 Value = temperature.Value
             }, cancellationToken);
         }
-    }
-
-    private static byte[] ObjectToByteArray(object obj)
-        => Encoding.UTF8.GetBytes(JsonSerializer.Serialize(obj));
-
-    private static async Task CompressThisShit(byte[] data, CancellationToken stoppingToken)
-    {
-        using var compressedStream = new MemoryStream();
-        await using var compressor = new GZipStream(compressedStream, CompressionLevel.SmallestSize);
-
-        await compressor.WriteAsync(data, 0, data.Length, stoppingToken);
-        await compressor.FlushAsync(stoppingToken);
-        var compressedData = compressedStream.ToArray();
-
-        //using MemoryStream originalStream = new MemoryStream(data, false);
-        //await originalStream.CopyToAsync(compressor, stoppingToken);
-
-        //using var ms = new MemoryStream();
-        //await compressedStream.CopyToAsync(ms, stoppingToken);
-        //var compressedByteArray = ms.ToArray();
     }
 }
