6f0262e96a4268e5d42b0fdbc61db384154e2000
jfbueno <buenojeferson@outlook.com>
2023-05-26 17:11:35 -0300

    Novas replicas

--- start-replicas-extras.ps1
+++ start-replicas-extras.ps1
@@ -1,4 +1,5 @@
 docker compose -f .\docker-compose.yaml `
     -f .\docker-compose-nodes.yaml `
     -f .\docker-compose-replicas.yaml `
+    -f .\docker-compose-replicas-extras.yaml `
     up -d --build
--- stop-replicas-extras.ps1
+++ stop-replicas-extras.ps1
@@ -1,4 +1,5 @@
 docker compose -f .\docker-compose.yaml `
     -f .\docker-compose-nodes.yaml `
     -f .\docker-compose-replicas.yaml `
-    up -d --build
+    -f .\docker-compose-replicas-extras.yaml `
+    down
